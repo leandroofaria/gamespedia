1f68f5d4a26fee18b0d2e8544394d579e19d463d
leandroofaria <leandrogabrielfaria05@gmail.com>
2025-03-11 12:22:16 -0300

    Gerando Jogo do Dia

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,12 @@ import { GameProps } from '@/utils/types/game'
 import Link from 'next/link'
 import Image from 'next/image'
 
+import { BsArrowRightSquare } from 'react-icons/bs'
+
 
 async function getGame () {
   try {
-    const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game_day`)
+    const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game_day`, {next: {revalidate:320}})
     return res.json()
   } catch {
     throw new Error("Falha ao pegar os dados")
@@ -21,15 +23,22 @@ export default async function Home() {
     <main className="w-full">
       <Container>
         <Link href={`/game/${game.id}`}>
-          <section className="w-full bg-black rounded-lg">
-            <Image 
-            src={game.image_url}
-            alt={game.title}
-            priority={true}
-            quality={100}
-            width={100}
-            height={100}
-            />
+          <section className="w-full bg-black rounded-lg mt-8">
+            <div className=" w-full max-h-72 h-72 relative rounded-lg ">
+              <div className="absolute z-20 w-full flex justify-between p-5">
+                <p className="font-bold text-xl text-white">{game.title}</p>
+                <BsArrowRightSquare size={24} color="#FFF"/>
+              </div>
+              <Image 
+              src={game.image_url}
+              alt={game.title}
+              priority={true}
+              quality={100}
+              fill={true}
+              sizes="(max-width:768px) 100vw, (max-width:1200px) 33vw"
+              className="max-h-72 object-cover rounded-lg opacity-50 hover:opacity-70 transition-all duration-300" 
+              />
+            </div>
           </section>
         </Link>
       </Container>
